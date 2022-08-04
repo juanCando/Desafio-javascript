c7b137b46819882c7cad331e37d9f2e3e82d8b4e
Juan cando <juancandocodea@gmail.com>
2022-08-03 23:43:06 -0300

    entregable

--- main.js
+++ main.js
@@ -24,7 +24,9 @@ class Alumno {
     	sum = sum + asign.getCalificacion();
     })
         
-  	return sum / this.asignaturas.length;
+  	let resultado = sum / this.asignaturas.length;
+    return resultado.toFixed(2)
+
   }
 }
 
@@ -56,22 +58,32 @@ class Asignatura {
 
 let alumnos = [];
 const nombresAsignaturas = ['Matemáticas', "Ciencias Sociales", "Ética y Ciudadanía"];
+const PromedioDeMaterias = [0,0,0]
+let contador = 0
 
 let legajo;
 let nombre = prompt('Ingrese el nombre del alumno (o presione ESC para finalizar)');
-while (nombre) {
+while (nombre != "ESC") {
   legajo = prompt('Ingrese el legajo del alumno');
   let asignaturas = [];
   
 	nombresAsignaturas.forEach((nombre) => {
-  	let calificacion = prompt(`Ingrese la calificación obtenida para ${nombre}`);
-    if (typeof calificacion !== 'number' && (calificacion < 0 || calificacion > 10)) {
+  	let calificacion = parseInt(prompt(`Ingrese la calificación obtenida para ${nombre}`));
+    if (calificacion !== 'number' && (calificacion < 0 || calificacion > 10)) {
     	alert('Error en la calificacion');
       throw '';
     }
-    
+    if (nombre === "Matemáticas"){
+      PromedioDeMaterias[0] = PromedioDeMaterias[0] + calificacion
+    }else if (nombre === "Ciencias Sociales"){
+      PromedioDeMaterias[1] = PromedioDeMaterias[1] + calificacion
+    }else{
+      PromedioDeMaterias[2] = PromedioDeMaterias[2] + calificacion
+    }
+    contador++
     const asignatura = new Asignatura(nombre, calificacion);
     asignaturas.push(asignatura);
+    console.log(PromedioDeMaterias)
   });
 
 	alumnos.push(new Alumno(nombre, legajo, asignaturas));
@@ -81,12 +93,19 @@ while (nombre) {
 
 
 alumnos.forEach((alumno) => {
-  alert(`Para el alumno ${alumno.getNombre()}, se tienen las siguientes notas: \n`);
+  console.log(`Para el alumno ${alumno.getNombre()}, se tienen las siguientes notas: \n`);
 	alumno.getAsignaturas().forEach((asignatura) => {
-    alert(`${asignatura.getNombre()} - nota: ${asignatura.getCalificacion()}`);
+    console.log(`${asignatura.getNombre()} - nota: ${asignatura.getCalificacion()}`);
   })
-	alert(`El alumno ${alumno.getNombre()} tuvo un promedio de ${alumno.getPromedio()}`)
+	console.log(`El alumno ${alumno.getNombre()} tuvo un promedio de ${alumno.getPromedio()}`)
 });
 
+console.log(`el promedio de matematicas con las notas generales de alumnos es ${PromedioDeMaterias[0]/contador.toFixed(2)}`)
+console.log(`el promedio de Ciencias Sociales con las notas generales de alumnos es ${PromedioDeMaterias[1]/contador.toFixed(2)}`)
+console.log(`el promedio de Ética y Ciudadanía con las notas generales de alumnos es ${PromedioDeMaterias[2]/contador.toFixed(2)}`)
 
- 
+
+
+
+
+
